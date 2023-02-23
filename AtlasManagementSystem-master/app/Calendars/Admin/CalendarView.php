6ea3be5d94475ad316ebd363a702468f3274619f
Hiroto Horigome <hiroto.horigome0528cc@gmail.com>
2023-02-23 12:10:23 +0900

    対象画面：スクール予約画面 改修内容： ■スクール予約表の過去日は下記の表示形式にする。 ・背景色をグレーにする ・受付終了の文言を表示する 対象画面：スクール予約画面 改修内容： ■スクール予約表の過去日は下記の表示形式にする。 ・背景色をグレーにする ・受付終了の文言を表示する

--- AtlasManagementSystem-master/app/Calendars/Admin/CalendarView.php
+++ AtlasManagementSystem-master/app/Calendars/Admin/CalendarView.php
@@ -1,20 +1,32 @@
 <?php
+
 namespace App\Calendars\Admin;
+
 use Carbon\Carbon;
 use App\Models\Users\User;
 
-class CalendarView{
+class CalendarView
+{
+  // ①ライブラリ Carbon を使うために、クラス内で $carbon という名前のプライベート変数を宣言するコードです。
   private $carbon;
 
-  function __construct($date){
+  // Carbonライブラリのインスタンスを作成し、$date 変数で初期化した後、現在のクラスの $carbon プロパティに代入するコードです。
+  function __construct($date)
+  {
     $this->carbon = new Carbon($date);
   }
+  // ここまでがCarbonライブラリを使うまでの準備
 
-  public function getTitle(){
+  // 現在の年月を取得　例）2023年2月
+  public function getTitle()
+  {
     return $this->carbon->format('Y年n月');
   }
 
-  public function render(){
+
+  public function render()
+  {
+    // カレンダーの表(外側)を作成している
     $html = [];
     $html[] = '<div class="calendar text-center">';
     $html[] = '<table class="table m-auto border">';
@@ -31,20 +43,28 @@ class CalendarView{
     $html[] = '</thead>';
     $html[] = '<tbody>';
 
+    // カレンダーの中身を作成している
+    // 週を取得
     $weeks = $this->getWeeks();
-
-    foreach($weeks as $week){
-      $html[] = '<tr class="'.$week->getClassName().'">';
+    // 週を表示させている
+    foreach ($weeks as $week) {
+      $html[] = '<tr class="' . $week->getClassName() . '">';
       $days = $week->getDays();
-      foreach($days as $day){
+      // 日を表示させる
+      foreach ($days as $day) {
+        // 月の最初の日を$startDayに代入
         $startDay = $this->carbon->format("Y-m-01");
+        // 今日の日付を＄toDayに代入
         $toDay = $this->carbon->format("Y-m-d");
-        if($startDay <= $day->everyDay() && $toDay >= $day->everyDay()){
+        // その日が現在の月の範囲内にあるかどうかを判断している。
+        if ($startDay <= $day->everyDay() && $toDay >= $day->everyDay()) {
           $html[] = '<td class="past-day border">';
-        }else{
-          $html[] = '<td class="border '.$day->getClassName().'">';
+        } else {
+          $html[] = '<td class="border ' . $day->getClassName() . '">';
         }
+        // カレンダーの中身(日付)を作成
         $html[] = $day->render();
+        // スクールの残り枠数を表示している？
         $html[] = $day->dayPartCounts($day->everyDay());
         $html[] = '</td>';
       }
@@ -56,15 +76,19 @@ class CalendarView{
 
     return implode("", $html);
   }
-
-  protected function getWeeks(){
+  // その月の週を取得している
+  protected function getWeeks()
+  {
     $weeks = [];
     $firstDay = $this->carbon->copy()->firstOfMonth();
     $lastDay = $this->carbon->copy()->lastOfMonth();
+    // 最初の週のカレンダー週 ($week) を作成し、カレンダー週の配列 ($weeks) に追加する
     $week = new CalendarWeek($firstDay->copy());
     $weeks[] = $week;
+    // ◉2週目以降、1週間後の日付 ($tmpDay) を取得し、その週のカレンダー週を作成し、カレンダー週の配列に追加する。
     $tmpDay = $firstDay->copy()->addDay(7)->startOfWeek();
-    while($tmpDay->lte($lastDay)){
+    // 最後の日付に到達するまで、◉を繰り返す。
+    while ($tmpDay->lte($lastDay)) {
       $week = new CalendarWeek($tmpDay, count($weeks));
       $weeks[] = $week;
       $tmpDay->addDay(7);
