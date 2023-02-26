23d7b877dad89edd35049a37aee0f3b5bd863539
Hiroto Horigome <hiroto.horigome0528cc@gmail.com>
2023-02-26 18:27:50 +0900

    バリデーション完了

--- AtlasManagementSystem-master/resources/lang/ja/validation.php
+++ AtlasManagementSystem-master/resources/lang/ja/validation.php
@@ -95,7 +95,7 @@ return [
     'numeric'              => ':attributeには、数字を指定してください。',
     'present'              => ':attributeが存在していません。',
     'regex'                => ':attributeに正しい形式を指定してください。',
-    'required'             => ':attributeは必ず指定してください。',
+    'required'             => ':attributeは必ず入力してください。',
     'required_if'          => ':otherが:valueの場合、:attributeも指定してください。',
     'required_unless'      => ':otherが:valuesでない場合、:attributeを指定してください。',
     'required_with'        => ':valuesを指定する場合は、:attributeも指定してください。',
@@ -152,6 +152,7 @@ return [
         'main_category_name' => 'メインカテゴリー',
         'main_category_id' => 'メインカテゴリー',
         'sub_category_name' => 'サブカテゴリー',
+        'comment' => 'コメント',
     ],
 
 ];
